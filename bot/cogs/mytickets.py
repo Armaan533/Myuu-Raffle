a46257ceb279d252e846e86584cb631577452e35
Armaan <mohammadarmaan533@gmail.com>
2022-05-12 10:58:39 +0530

    added new cog with mytickets command

---
+++ bot/cogs/mytickets.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+import logical_definitions as lgd
+import mongo_declarations as mn
+
+class mytickets(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command
+    async def mytickets(self, ctx):
+        if str(ctx.guild.id) not in mn.raffledbase.list_collection_names():
+            await ctx.send(embed = discord.Embed(
+                title = "No raffle found",
+                description = "No raffle has been created for this guild",
+                color = lgd.hexConvertor(mn.colorCollection.find({},{"_id":0,"Hex":1}))
+            ))
+
+        else:
+            authorid = ctx.author.id
+            guildraffle = mn.raffledbase[str(ctx.guild.id)]
+            tickets = guildraffle.find_one({"_id":authorid},{"_id":0,"tickets":1})["tickets"]
+            rafflename = guildraffle.find_one({"_id":"Raffle"},{"_id":0,"RaffleName":1})["RaffleName"]
+            await ctx.send(embed = discord.Embed(
+                title = "Tickets",
+                description = f"You have bought {tickets} ticket(s) for raffle {rafflename}",
+                color = lgd.hexConvertor(mn.colorCollection.find({},{"_id":0,"Hex":1}))
+            ))
+
+def setup(client):
+    client.add_cog(mytickets(client))
