1617243495a9dfe75666c9051df1b37ed5553dc9
Armaan <77953840+Armaan533@users.noreply.github.com>
2022-05-22 10:20:58 +0000

    fixing the bot for replit

--- bot/mongo_declarations.py
+++ bot/mongo_declarations.py
@@ -1,7 +1,10 @@
 import pymongo
 import os
 
-mongoclient = pymongo.MongoClient("mongodb://armaan533s:duBE7GLlj6YVAnOH@cluster0-shard-00-00.tw8fv.mongodb.net:27017,cluster0-shard-00-01.tw8fv.mongodb.net:27017,cluster0-shard-00-02.tw8fv.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-1x45s3-shard-0&authSource=admin&retryWrites=true&w=majority",serverSelectionTimeoutMs = 5000)
+current = os.getcwd()
+currentFiles = os.listdir(current)
+if ".replit" in currentFiles:
+    mongoclient = pymongo.MongoClient(os.getenv('mongo_db_link'),serverSelectionTimeoutMs = 5000)
 
 hexdbase = mongoclient["hex_colours"]
 colorCollection = hexdbase["colours"]
