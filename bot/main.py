c591b94f96b986d2594dd401202d7052b41f3fd4
Armaan <mohammadarmaan533@gmail.com>
2022-06-05 16:10:19 +0530

    Hopefully fixed the raffleinfo command

--- bot/main.py
+++ bot/main.py
@@ -368,7 +368,7 @@ async def raffleinfo(ctx):
 				aboutimg.save(f"bot/Images/info{ctx.guild.id}.png", format="png")
 				# await lgd.save_image(f"app/bot/Images/info{RaffleName}.png", buffer.getbuffer())
 
-				Imgfile = discord.File(path = f"bot/Images/info{ctx.guild.id}.png", filename = f"info{ctx.guild.id}.png")
+				Imgfile = discord.File(f"bot/Images/info{ctx.guild.id}.png", filename = f"info{ctx.guild.id}.png")
 				infoEmbed.set_image(url = f"attachment://info{ctx.guild.id}.png")
 			else:
 				infoEmbed.set_image(url = f"attachment://info{ctx.guild.id}.png")
