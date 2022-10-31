23faafcac6d9a434fc54792b4122828209830aa7
Armaan <mohammadarmaan533@gmail.com>
2022-10-31 12:41:13 +0530

    fixing bot

--- bot/bot_env_vars.py
+++ bot/bot_env_vars.py
@@ -1,2 +1,2 @@
-token = "OTQ1MzAxNTE0OTQ2MjQ0NjI5.GT9cer.7XC-lfPckOCcqcCE3BiMOG-Qo9VFsiezAdunA8"
+token = "OTQ1MzAxNTE0OTQ2MjQ0NjI5.GNaA-v.4vvnaIYkAtNAB4CDhKroFiyCdLYQlhgbwcDUpo"
 mongo_db_link = "mongodb://armaan533s:v9TTdhskeOmxI5A0@cluster0-shard-00-00.tw8fv.mongodb.net:27017,cluster0-shard-00-01.tw8fv.mongodb.net:27017,cluster0-shard-00-02.tw8fv.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-1x45s3-shard-0&authSource=admin&retryWrites=true&w=majority"
